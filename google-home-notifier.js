be6e820c012a65794cc1520b5055f53bfff6a3a4
thunder <thunder@debianserver.thunderace.fr>
2017-11-21 09:42:09 +0100

    keep minimal dependencies

--- google-home-notifier.js
+++ google-home-notifier.js
@@ -1,72 +1,33 @@
 var Client = require('castv2-client').Client;
 var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
-var mdns = require('mdns');
-var browser = mdns.createBrowser(mdns.tcp('googlecast'));
+var googletts = require('google-tts-api');
+
 var deviceAddress;
 var language;
 
-var device = function(name, lang = 'en') {
-  device = name;
+var device = function(ip, lang = 'en') {
+  deviceAddress = ip;
   language = lang;
   return this;
 };
 
-var ip = function(ip) {
-  deviceAddress = ip;
-  return this;
-}
-
-var googletts = require('google-tts-api');
-var googlettsaccent = 'us';
-var accent = function(accent) {
-  googlettsaccent = accent;
-  return this;
-}
 
 var notify = function(message, callback = function() {}) {
-  if (!deviceAddress){
-    browser.start();
-    browser.on('serviceUp', function(service) {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(device.replace(' ', '-'))){
-        deviceAddress = service.addresses[0];
-        getSpeechUrl(message, deviceAddress, function(res) {
-          callback(res);
-        });
-      }
-      browser.stop();
-    });
-  }else {
-    getSpeechUrl(message, deviceAddress, function(res) {
-      callback(res);
-    });
-  }
+  getSpeechUrl(message, deviceAddress, function(res) {
+    callback(res);
+  });
 };
 
 var play = function(mp3_url, callback = function() {}) {
-  if (!deviceAddress) {
-    browser.start();
-    browser.on('serviceUp', function(service) {
-      console.log('Device "%s" at %s:%d', service.name, service.addresses[0], service.port);
-      if (service.name.includes(device.replace(' ', '-'))){
-        deviceAddress = service.addresses[0];
-        getPlayUrl(mp3_url, deviceAddress, function(res) {
-          callback(res);
-        });
-      }
-      browser.stop();
-    });
-  } else {
-    getPlayUrl(mp3_url, deviceAddress, function(res) {
-      callback(res);
-    });
-  }
+  getPlayUrl(mp3_url, deviceAddress, function(res) {
+    callback(res);
+  });
 };
 
 var getSpeechUrl = function(text, host, callback) {
   googletts(text, language, 1).then(function (url) {
     onDeviceUp(host, url, function(res){
-      callback(res)
+      callback(res);
     });
   }).catch(function (err) {
     console.error(err.stack);
@@ -75,7 +36,7 @@ var getSpeechUrl = function(text, host, callback) {
 
 var getPlayUrl = function(url, host, callback) {
   onDeviceUp(host, url, function(res){
-    callback(res)
+    callback(res);
   });
 };
 
@@ -83,16 +44,21 @@ var onDeviceUp = function(host, url, callback) {
   var client = new Client();
   client.connect(host, function() {
     client.launch(DefaultMediaReceiver, function(err, player) {
-
-      var media = {
-        contentId: url,
-        contentType: 'audio/mp3',
-        streamType: 'BUFFERED' // or LIVE
-      };
-      player.load(media, { autoplay: true }, function(err, status) {
-        client.close();
-        callback('Device notified');
-      });
+      if (!err) {
+        var media = {
+          contentId: url,
+          contentType: 'audio/mp3',
+          streamType: 'BUFFERED' // or LIVE
+        };
+        player.load(media, { autoplay: true }, function(err, status) {
+          client.close();
+          if (!err) {
+            callback('Device notified');
+          } else {
+            callback('Device notification failed!');
+          }
+        });
+      }
     });
   });
 
@@ -103,8 +69,6 @@ var onDeviceUp = function(host, url, callback) {
   });
 };
 
-exports.ip = ip;
 exports.device = device;
-exports.accent = accent;
 exports.notify = notify;
 exports.play = play;
